7ac7358b06e6b90cc766c761625af7b2828b5f11
NicolaRuggiero <nicoruggi97@live.it>
2021-12-02 15:19:28 +0100

    Added Firebase Integration

--- src/app/history/history.service.ts
+++ src/app/history/history.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import {ValueSensors} from '../home/valueSensors.model';
 import * as HighCharts from 'highcharts';
 import { HttpClient } from '@angular/common/http';
 import {map , tap} from 'rxjs/operators';
@@ -11,14 +10,67 @@ import {map , tap} from 'rxjs/operators';
 export class HistoryService {
 
   size : number;
+  saturation :number [];
 
   constructor(private httpClient: HttpClient) { }
   
-  fetchData(){
-	  return this.httpClient.get("https://neptune-ad095.firebaseio.com/size.json")
+  fetchSize(){
+	    return this.httpClient.get("https://neptune-ad095.firebaseio.com/size.json")
      .pipe(tap(resData => {
      this.size = (Number(resData));
-     })).subscribe();
+     console.log(this.size);
+     }));
+     
   }
 
-}
+  fetchSaturation(){
+      console.log(this.size);
+      for(let i =0 ; i< this.size; i++){
+       this.getSaturation(i).subscribe();
+    }
+     
+   }
+
+   getSaturation(i:number){
+
+    return this.httpClient.get("https://neptune-ad095.firebaseio.com/saturation/" + String(i) + "/Data.json")
+      .pipe(tap(resData => {
+      this.saturation[i] = (Number(resData));
+      console.log(this.saturation[i]);
+      }));
+   }
+
+   plotSimpleBarChart() {
+    let myChart = HighCharts.chart('highcharts', {
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Saturation and heartrate history'
+      },
+      xAxis: {
+        
+      },
+      yAxis: {
+        title: {
+          text: ''
+        }
+      },
+      series: [
+        {
+          name: 'saturation',
+          type: undefined,
+          data: [this.saturation]
+        },
+        {
+          name: 'heartrate',
+          type: undefined,
+          data: [5, 7, 3]
+        }]
+    });
+  }
+
+  
+ }   
+
+
